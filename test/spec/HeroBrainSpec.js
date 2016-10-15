606ec331eeb39d664c7fe0dd43c32e463301b69f
John Krull <astrom.flux@gmail.com>
2016-10-15 07:43:44 +0000

    Clean up test spec

--- test/spec/HeroBrainSpec.js
+++ test/spec/HeroBrainSpec.js
@@ -1,31 +1,28 @@
+/* eslint no-undef: 0 */
 var chai = require('chai');
-var should = chai.should();
-var expect = chai.expect;
+
+chai.should();
 
 describe('Hero file', function () {
-  'use strict';
-  var move; 
+    var move;
 
-  beforeEach(function() {
-    move = require('../../hero.js');
-  });
+    beforeEach(function () {
+        move = require('../../hero.js');
+    });
 
-  it('exists and exports a move function', function () {
-    move.should.be.a('function');
-  });
-
-
+    it('exists and exports a move function', function () {
+        move.should.be.a('function');
+    });
 });
+
 describe('Helper file', function () {
-  'use strict';
-  var helpers;
+    var helpers;
 
-  beforeEach(function() {
-    helpers = require('../../helpers.js');
-  });
+    beforeEach(function () {
+        helpers = require('../../helpers.js');
+    });
 
-  it('exists and exports a helper object', function () {
-    helpers.should.be.a('object');
-  });
-
+    it('exists and exports a helper object', function () {
+        helpers.should.be.a('object');
+    });
 });
